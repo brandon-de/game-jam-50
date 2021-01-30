2fd005747c68e482382ff6179d878217b3233739
Brandon DeCook <brandondecook@gmail.com>
2021-01-30 13:18:25 -0500

    prevent client side caching

--- webpack/prod.js
+++ webpack/prod.js
@@ -6,7 +6,8 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge(base, {
   mode: "production",
   output: {
-    filename: "bundle.min.js"
+    //filename: "bundle.min.js"
+    filename: '[name].[contenthash].js'
   },
   devtool: false,
   performance: {
